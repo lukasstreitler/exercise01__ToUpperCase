b7fe5a6532ba7ab6cf6620430d02cfa2d1fd5562
Mathias Rieder <mathias.rieder@gmail.com>
2020-04-29 08:47:48 +0000

    implemented toUpperCase

--- UpperCase.c
+++ UpperCase.c
@@ -8,7 +8,13 @@
  * turns the given String into upper-case characters
 */
 void toUpperCase(char* text){
-    
+    // put your code here!
+    while(*text != '\0'){
+        if (*text >= 'a' && *text <= 'z'){
+            *text = *text - 32;
+        }
+        text++;
+    }
 }
 
 void test(char* text){
@@ -23,6 +29,8 @@ int main(int argc, char const *argv[]) {
 
   test("xyz");
   test("this is a test");
+  test("this IS a test!"); // THIS IS A TEST!
+  test("numbers 123"); // NUMBERS 123
   
   return 0;
 }
